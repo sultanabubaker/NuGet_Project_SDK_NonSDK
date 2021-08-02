e0af537ecc5415baf509454daa0eeb3423174c09
SultanAbuBaker <m8coollg@gmail.com>
2021-08-02 13:56:00 +0300

    Commit message

---
+++ SDK2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
